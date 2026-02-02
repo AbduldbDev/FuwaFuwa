dc887ef2a9b794becc241fa6660e4be9e8b7655d
Abdul De Borja <abduldbdev@gmail.com>
2026-02-02 15:05:47 +0800

    fix(JS): Fix JS

--- resources/views/Pages/assets.blade.php
+++ resources/views/Pages/assets.blade.php
@@ -141,7 +141,7 @@
         </div>
     </section>
 
-    <script src="{{ asset('./js/Assets/assetFilter.js') }}"></script>
+    <script src="{{ asset('js/Assets/assetFilter.js') }}"></script>
 @endsection
 
 @push('css')
